4bbe02f7b5c733f63879bdba42e7da3550ba2aa4
Joseph Antony <techweed@users.noreply.github.com>
2021-12-28 04:13:58 +0530

    fixes

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import Home from './src/Home';
+import Home from './src/Home/Home';
 import Detail from './src/Detail/Detail';
 
 const Stack = createNativeStackNavigator();
